14d62c377f1d0b88fce108526b2573c18d6c106d
StevenNguyen0501 <56420662+StevenNguyen0501@users.noreply.github.com>
2024-01-23 13:42:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ hide_menu_style = """
         footer {visibility: hidden;}
         </style>
         """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.markdown(hide_menu_style, unsafe_allow_html=False)
 
 # Replicate Credentials
 # App title
